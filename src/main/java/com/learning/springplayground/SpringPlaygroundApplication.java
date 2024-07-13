45456163399579d15e2a567018249e26cf211dfb
Jimyeong Kim <kjm021221@icloud.com>
2024-07-14 00:07:50 +0900

    first commit

---
+++ src/main/java/com/learning/springplayground/SpringPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/springplayground/SpringPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
